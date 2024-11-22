572bcb42a29493b7c74974de4fea85c7e2adabae
marianoluiz <kmarianu@gmail.com>
2024-11-23 00:39:09 +0800

    modified file structure

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Home = () => {
   return (
     <>
-      <h1>Welcome to the Multi-Page Form App</h1>
-      <NavLink to="/form-page-1">Start the Form</NavLink>
+      <h1>Welcome to Pamantasan ng Lungsod ng Maynila</h1>
+      <NavLink to="/form-page-1">Register Now</NavLink>
     </>
   );
 }
